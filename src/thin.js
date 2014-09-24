cb28a0a1427d36061abd45fdf86074ae69a99cab
xufei <xu.fei@outlook.com>
2014-09-24 23:56:02 +0800

    小调整

--- src/thin.js
+++ src/thin.js
@@ -1,319 +1,318 @@
 "use strict";
 
 (function (win, doc, _) {
-	var readyFunctions = [];
+    var readyFunctions = [];
 
-	var noop = function () {
-	};
+    var noop = function () {
+    };
 
-	var uuid = function () {
-		return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-			var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-			return v.toString(16);
-		});
-	};
+    var uuid = function () {
+        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    };
 
-	var Events = {
-		$on: function (eventType, handler) {
-			if (!this.eventMap) {
-				this.eventMap = {};
-			}
+    var Events = {
+        $on: function (eventType, handler) {
+            if (!this.eventMap) {
+                this.eventMap = {};
+            }
 
-			//multiple event listener
-			if (!this.eventMap[eventType]) {
-				this.eventMap[eventType] = [];
-			}
-			this.eventMap[eventType].push(handler);
-		},
+            //multiple event listener
+            if (!this.eventMap[eventType]) {
+                this.eventMap[eventType] = [];
+            }
+            this.eventMap[eventType].push(handler);
+        },
 
-		$off: function (eventType, handler) {
-			for (var i = 0; i < this.eventMap[eventType].length; i++) {
-				if (this.eventMap[eventType][i] === handler) {
-					this.eventMap[eventType].splice(i, 1);
-					break;
-				}
-			}
-		},
+        $off: function (eventType, handler) {
+            for (var i = 0; i < this.eventMap[eventType].length; i++) {
+                if (this.eventMap[eventType][i] === handler) {
+                    this.eventMap[eventType].splice(i, 1);
+                    break;
+                }
+            }
+        },
 
-		$fire: function (event) {
-			var eventType = event.type;
-			if (this.eventMap && this.eventMap[eventType]) {
-				for (var i = 0; i < this.eventMap[eventType].length; i++) {
-					this.eventMap[eventType][i](event);
-				}
-			}
-		}
-	};
+        $fire: function (event) {
+            var eventType = event.type;
+            if (this.eventMap && this.eventMap[eventType]) {
+                for (var i = 0; i < this.eventMap[eventType].length; i++) {
+                    this.eventMap[eventType][i](event);
+                }
+            }
+        }
+    };
 
-	var thin = function () {
-	};
+    var thin = function () {
+    };
 
-	_.extend(thin, Events);
+    _.extend(thin, Events);
 
-	_.extend(thin, {
-		ready: function (handler, priority) {
-			priority = priority || 1;
+    _.extend(thin, {
+        ready: function (handler, priority) {
+            priority = priority || 1;
 
-			if (!readyFunctions[priority]) {
-				readyFunctions[priority] = [];
-			}
-			readyFunctions[priority].push(handler);
-		},
+            if (!readyFunctions[priority]) {
+                readyFunctions[priority] = [];
+            }
+            readyFunctions[priority].push(handler);
+        },
 
-		error: function () {
+        error: function () {
 
-		},
+        },
 
-		log: function (obj) {
-			try {
-				console.log(obj);
-			}
-			catch (ex) {
+        log: function (obj) {
+            try {
+                console.log(obj);
+            }
+            catch (ex) {
 
-			}
-		}
-	});
+            }
+        }
+    });
 
-	var addListener = doc.addEventListener || doc.attachEvent,
-		removeListener = doc.removeEventListener || doc.detachEvent;
+    var addListener = doc.addEventListener || doc.attachEvent,
+        removeListener = doc.removeEventListener || doc.detachEvent;
 
-	var eventName = doc.addEventListener ? "DOMContentLoaded" : "onreadystatechange";
+    var eventName = doc.addEventListener ? "DOMContentLoaded" : "onreadystatechange";
 
-	addListener.call(doc, eventName, function () {
-		for (var i = readyFunctions.length - 1; i >= 0; i--) {
-			if (readyFunctions[i]) {
-				for (var j = 0; j < readyFunctions[i].length; j++) {
-					readyFunctions[i][j]();
-				}
-			}
-		}
-	}, false);
+    addListener.call(doc, eventName, function () {
+        for (var i = readyFunctions.length - 1; i >= 0; i--) {
+            if (readyFunctions[i]) {
+                for (var j = 0; j < readyFunctions[i].length; j++) {
+                    readyFunctions[i][j]();
+                }
+            }
+        }
+    }, false);
 
-	var bindingMap = {};
-	var changeQueue = [];
+    var bindingMap = {};
+    var changeQueue = [];
 
-	function enqueue(item) {
-		changeQueue.push(item);
-	}
+    function enqueue(item) {
+        changeQueue.push(item);
+    }
 
-	function addBinding(vm, key, handler) {
-		if (!bindingMap[vm]) {
-			bindingMap[vm] = {};
-		}
+    function addBinding(vm, key, handler) {
+        if (!bindingMap[vm]) {
+            bindingMap[vm] = {};
+        }
 
-		if (!bindingMap[vm][key]) {
-			bindingMap[vm][key] = [];
-		}
+        if (!bindingMap[vm][key]) {
+            bindingMap[vm][key] = [];
+        }
 
-		bindingMap[vm][key].push(handler);
-	}
+        bindingMap[vm][key].push(handler);
+    }
 
-	function performChange() {
-		window.setTimeout(function() {
-			changeQueue.forEach(function(item) {
-				item.handler(item.value);
-			});
-			changeQueue = [];
-		}, 0);
-	}
+    function performChange() {
+        window.setTimeout(function () {
+            changeQueue.forEach(function (item) {
+                item.handler(item.value);
+            });
+            changeQueue = [];
+        }, 0);
+    }
 
-	thin.$watch = function (model) {
-		Object.observe(model, function (changes) {
-			changes.forEach(function (change, i) {
-				console.log(change);
-				bindingMap[change.object][change.name].forEach(function(handler) {
-					enqueue({
-						handler: handler,
-						value: change.object[change.name]
-					});
-				});
-			});
-		});
-	};
+    thin.$watch = function (model) {
+        Object.observe(model, function (changes) {
+            changes.forEach(function (change, i) {
+                console.log(change);
+                bindingMap[change.object][change.name].forEach(function (handler) {
+                    enqueue({
+                        handler: handler,
+                        value: change.object[change.name]
+                    });
+                });
+            });
+        });
+    };
 
-	var parser = (function () {
-		function parseElement(element, vm) {
-			var model = vm;
+    var parser = (function () {
+        var binding = {
+            value: function (element, key, vm) {
+                thin.log("binding value: " + key);
 
-			if (element.getAttribute("vm-model")) {
-				model = bindModel(element.getAttribute("vm-model"));
-			}
+                addBinding(vm, key, function (value) {
+                    element.value = value || "";
+                });
 
-			var i;
-			for (i = 0; i < element.attributes.length; i++) {
-				parseAttribute(element, element.attributes[i], model);
-			}
+                switch (element.tagName) {
+                    case "SELECT":
+                    {
+                        bindSelectValue(element, key, vm);
+                        break;
+                    }
+                    default:
+                    {
+                        bindTextValue(element, key, vm);
+                        break;
+                    }
+                }
 
-			for (i = 0; i < element.children.length; i++) {
-				parseElement(element.children[i], model);
-			}
+                function bindTextValue(el, key, model) {
+                    el.onkeyup = el.onpaste = el.onkeypress = function () {
+                        model[key] = el.value;
+                        performChange();
+                    };
+                }
 
-			if (model && model.$initializer) {
-				model.$initializer();
-			}
-		}
+                function bindSelectValue(el, key, model) {
+                    el.onchange = function () {
+                        vm[key] = el.value;
+                        performChange();
+                    };
+                }
+            },
+            list: function (element, key, vm) {
+                thin.log("binding list: " + key);
 
-		function parseAttribute(element, attr, model) {
-			if (attr.name.indexOf("vm-") === 0) {
-				var type = attr.name.slice(3);
+                vm.$watch(key, function (value, oldValue) {
+                    var selectedValue = element.value;
+                    element.innerHTML = null;
 
-				switch (type) {
-					case "init":
-						bindInit(element, attr.value, model);
-						break;
-					case "value":
-						bindValue(element, attr.value, model);
-						break;
-					case "list":
-						bindList(element, attr.value, model);
-						break;
-					case "click":
-						bindClick(element, attr.value, model);
-						break;
-					case "enable":
-						bindEnable(element, attr.value, model, true);
-						break;
-					case "disable":
-						bindEnable(element, attr.value, model, false);
-						break;
-					case "visible":
-						bindVisible(element, attr.value, model, true);
-						break;
-					case "invisible":
-						bindVisible(element, attr.value, model, false);
-						break;
-					case "element":
-						model[attr.value] = element;
-						break;
-				}
-			}
-		}
+                    for (var i = 0; i < value.length; i++) {
+                        var item = document.createElement("option");
+                        item.innerHTML = value[i].label;
+                        item.value = value[i].key;
 
-		function bindModel(name) {
-			thin.log("binding model: " + name);
+                        element.appendChild(item);
+                    }
+                    element.value = selectedValue;
+                });
+            },
+            init: function (element, key, vm) {
+                thin.log("binding init: " + key);
 
-			var vm = {};
-			thin.$watch(vm);
+                vm.$initializer = (function (model) {
+                    return function () {
+                        model[key]();
+                        performChange();
+                    };
+                })(vm);
+            },
 
-			var instance = new window[name]();
-			_.extend(vm, instance);
+            click: function (element, key, vm) {
+                thin.log("binding click: " + key);
 
-			return vm;
-		}
+                element.onclick = function () {
+                    vm[key]();
+                    performChange();
+                };
+            },
+            enable: function (element, key, vm, direction) {
+                thin.log("binding enable: " + key);
 
-		function bindValue(element, key, vm) {
-			thin.log("binding value: " + key);
+                if (typeof vm[key] == "function") {
+                    changeHandlers.push(function () {
+                        element.disabled = vm[key]() ^ direction ? true : false;
+                    });
+                }
+                else {
+                    vm.$watch(key, function (value, oldValue) {
+                        element.disabled = value ^ direction ? true : false;
+                    });
+                }
+            },
+            visible: function (element, key, vm, direction) {
+                thin.log("binding visible: " + key);
 
-			addBinding(vm, key, function(value) {
-				element.value = value || "";
-			});
+                if (typeof vm[key] == "function") {
+                    changeHandlers.push(function () {
+                        element.style.display = vm[key]() ^ direction ? "none" : "";
+                    });
+                }
+                else {
+                    vm.$watch(key, function (value, oldValue) {
+                        element.style.display = value ^ direction ? "none" : "";
+                    });
+                }
+            }
+        };
 
-			switch (element.tagName) {
-				case "SELECT": {
-					bindSelectValue(element, key, vm);
-					break;
-				}
-				default: {
-					bindTextValue(element, key, vm);
-					break;
-				}
-			}
 
-			function bindTextValue(el, key, model) {
-				el.onkeyup = function () {
-					model[key] = el.value;
-					performChange();
-				};
+        function bindModel(name) {
+            thin.log("binding model: " + name);
 
-				el.onpaste = function () {
-					model[key] = el.value;
-					performChange();
-				};
-			}
+            var vm = {};
+            thin.$watch(vm);
 
-			function bindSelectValue(el, key, model) {
-				el.onchange = function () {
-					vm[key] = el.value;
-					performChange();
-				};
-			}
-		}
+            var instance = new window[name]();
+            _.extend(vm, instance);
 
-		function bindList(element, key, vm) {
-			thin.log("binding list: " + key);
+            return vm;
+        }
 
-			vm.$watch(key, function (value, oldValue) {
-				var selectedValue = element.value;
-				element.innerHTML = null;
+        function parseElement(element, vm) {
+            var model = vm;
 
-				for (var i = 0; i < value.length; i++) {
-					var item = document.createElement("option");
-					item.innerHTML = value[i].label;
-					item.value = value[i].key;
+            if (element.getAttribute("vm-model")) {
+                model = bindModel(element.getAttribute("vm-model"));
+            }
 
-					element.appendChild(item);
-				}
-				element.value = selectedValue;
-			});
-		}
+            [].forEach.call(element.attributes, function(attribute) {
+                parseAttribute(element, attribute, model);
+            });
 
-		function bindInit(element, key, vm) {
-			thin.log("binding init: " + key);
+            [].forEach.call(element.children, function(child) {
+                parseElement(child, model);
+            });
 
-			vm.$initializer = (function (model) {
-				return function () {
-					model[key]();
-					performChange();
-				};
-			})(vm);
-		}
+            if (model && model.$initializer) {
+                model.$initializer();
+            }
+        }
 
-		function bindClick(element, key, vm) {
-			thin.log("binding click: " + key);
+        function parseAttribute(element, attr, model) {
+            if (attr.name.indexOf("vm-") === 0) {
+                var type = attr.name.slice(3);
 
-			element.onclick = function () {
-				vm[key]();
-				performChange();
-			};
-		}
+                if (binding[type]) {
+                    binding[type](element, attr.value, model);
+                }
+/*
+                switch (type) {
+                    case "init":
+                        bindInit(element, attr.value, model);
+                        break;
+                    case "value":
+                        bindValue(element, attr.value, model);
+                        break;
+                    case "list":
+                        bindList(element, attr.value, model);
+                        break;
+                    case "click":
+                        bindClick(element, attr.value, model);
+                        break;
+                    case "enable":
+                        bindEnable(element, attr.value, model, true);
+                        break;
+                    case "disable":
+                        bindEnable(element, attr.value, model, false);
+                        break;
+                    case "visible":
+                        bindVisible(element, attr.value, model, true);
+                        break;
+                    case "invisible":
+                        bindVisible(element, attr.value, model, false);
+                        break;
+                    case "element":
+                        model[attr.value] = element;
+                        break;
+                }*/
+            }
+        }
 
-		function bindEnable(element, key, vm, direction) {
-			thin.log("binding enable: " + key);
+        return {
+            parse: parseElement
+        };
+    })();
 
-			if (typeof vm[key] == "function") {
-				changeHandlers.push(function() {
-					element.disabled = vm[key]() ^ direction ? true : false;
-				});
-			}
-			else {
-				vm.$watch(key, function (value, oldValue) {
-					element.disabled = value ^ direction ? true : false;
-				});
-			}
-		}
-
-		function bindVisible(element, key, vm, direction) {
-			thin.log("binding visible: " + key);
-
-			if (typeof vm[key] == "function") {
-				changeHandlers.push(function() {
-					element.style.display = vm[key]() ^ direction ? "none" : "";
-				});
-			}
-			else {
-				vm.$watch(key, function (value, oldValue) {
-					element.style.display = value ^ direction ? "none" : "";
-				});
-			}
-		}
-
-		return {
-			parse: parseElement
-		};
-	})();
-
-	thin.ready(function() {
-		parser.parse(doc.body);
-		performChange();
-	});
+    thin.ready(function () {
+        parser.parse(doc.body);
+        performChange();
+    });
 })(window, document, _);
